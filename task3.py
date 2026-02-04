14836dbfc01381790337495b82a0333a96ee109a
Aditya Yadav <yadavadi367@gmail.com>
2026-02-04 16:27:02 +0530

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,7 @@
+#Store the principal amount, rate of interest, and time in different variables and then calculate the Simple Interest for 3 years. Formula: Simple Interest = P x R x T / 100
+
+P = 1000  # Principal amount
+R = 5     # Rate of interest
+T = 3     # Time in years
+simple_interest = (P * R * T) / 100
+print("Simple Interest:", simple_interest)
