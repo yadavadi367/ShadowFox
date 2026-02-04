93e2da6d79bfe7b18f2695c6c0e7ba2f74db070a
Aditya Yadav <yadavadi367@gmail.com>
2026-02-04 17:15:16 +0530

    Delete task1(numbers_3).py

--- task1(numbers_3).py
+++
@@ -1,7 +0,0 @@
-#If you cross a 490 meter long street in 7 minutes, calculate your speed in meters per second. Print the answer without any decimal point in it. Hint: Speed = Distance / Time
-
-distance = 490  # meters
-time = 7 * 60   # convert minutes to seconds
-
-speed = distance / time
-print(str(int(speed)) + " m/s")
