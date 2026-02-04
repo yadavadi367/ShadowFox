ed4a53a736fc657a5b8927e20f168742d403d9c9
Aditya Yadav <yadavadi367@gmail.com>
2026-02-04 21:43:19 +0530

    Delete Beginner/variables2.py

--- Beginner/variables2.py
+++
@@ -1,7 +0,0 @@
-#Create a variable called for and assign it a value 4. See what happens and find out the reason behind the behavior that you see.
-
-for = 4
-
-#RESULT
-#SyntaxError: invalid syntax
-#The reason for this error is that "for" is a reserved keyword in Python, used for creating loops. You cannot use it as a variable name. To fix this, you can choose a different variable name that is not a reserved keyword.
